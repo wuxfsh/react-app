b1e73274849d7c0783d15ff29554724fd15a36e2
Wu <xiaofan.wu@sap.com>
2019-05-22 23:08:00 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello World of React! <br />
+            Hello World of React! This changed by Michael Wu <br />
             <em>[running on a docker container by docker-compose]</em>
           </p>
           <a
